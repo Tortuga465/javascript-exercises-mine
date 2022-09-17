83f8b341c0bb230f6a292380c813a08b1f559259
Tortuga465 <tem9669@yandex.ru>
2022-09-17 16:11:53 +0300

    Very chill. 5th done.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num_start, num_end) {
+    if ((num_start*num_end<0) || (typeof(num_end)!="number") || (typeof(num_start)!="number"))
+    {
+        return "ERROR";
+    }
+    return_value = 0;
+    if(num_start>num_end)
+    {
+        temp = num_start;
+        num_start = num_end;
+        num_end = temp;
+    }
+    
 
+
+    for(i=num_start; i<=num_end; i++)
+    {
+        return_value += i;
+    }
+    return return_value;
 };
 
 // Do not edit below this line
