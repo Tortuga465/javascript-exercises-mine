704bcc1e5392ccbb28dd83d92a53f31d122ffa8e
Tortuga465 <tem9669@yandex.ru>
2022-09-17 15:46:33 +0300

    Had to get creative but nailed it! Fourth down.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list, ...args) {
+    args.forEach(arg => {
+            for (var i = 0; i < list.length; ++i)
+            {
+                if ((list[i]==arg)&&(typeof(arg) == typeof(list[i]))) list.splice(i,1);
+            }
+        });
+    return list;
 };
 
 // Do not edit below this line
