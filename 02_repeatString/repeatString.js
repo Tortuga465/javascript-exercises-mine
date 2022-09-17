276f165ba96a88ede47d7cfb9f1aa10dbae0ff13
Tortuga465 <tem9669@yandex.ru>
2022-09-17 14:51:33 +0300

    Second task done. Easy too.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    str_ret = "";
+    if(num<0) return ("ERROR");
+    while (num>0)
+    {
+        str_ret += str;
+        num--;
+    }
+    return str_ret;
 };
 
 // Do not edit below this line
