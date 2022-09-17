18db41e91dbeef57cbacad042db1c93bd7c37ac2
Tortuga465 <tem9669@yandex.ru>
2022-09-17 14:59:21 +0300

    Third task done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string_input) {
+    string_output = "";
+    size = string_input.length-1;
+    for (let i in string_input)
+    {
+        string_output += string_input[size-i];
+    }
+    return string_output;
 };
 
 // Do not edit below this line
