398faa50c64e4065078ede87eae842cf7085cd7c
Tortuga465 <tem9669@yandex.ru>
2022-09-19 14:03:02 +0300

    Task was also done in 5-10 minutes, very straightfowrard.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp_in) {
+  temp_out = (temp_in - 32) * 5 / 9;
+  temp_out = Math.round(temp_out * 10) / 10;
+  return temp_out;
 };
 
-const ctof = function() {
-
+const ctof = function(temp_in) {
+  temp_out = (temp_in * 9 / 5) + 32;
+  temp_out = Math.round(temp_out * 10) / 10;
+  return temp_out;
 };
 
 // Do not edit below this line
